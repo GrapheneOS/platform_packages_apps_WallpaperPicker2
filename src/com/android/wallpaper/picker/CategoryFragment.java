3efb683750cc98fefb90ab18ced607a52fea8a5b
chihhangchuang <chihhangchuang@google.com>
2020-04-17 02:06:25 +0800

    [1/3] Create BottomActionBarFragment for BottomActionBar
    - BottomActionBar will be avaible for extending fragments.
    - Extending fragemnts won't handle reset BottomActionBar anymore.
    - All the back key event for BottomActionBar will be handled by BottomActionBarFragment, won't distribute in fragments.
    
    Idea graph: https://screenshot.googleplex.com/aNQVz6Di7B7.png
    
    Test: Manually
    Bug: 151287328
    Change-Id: I36ed1725165bafd8624a24e9e4fe9cd4f15a846e

--- src/com/android/wallpaper/picker/CategoryFragment.java
+++ src/com/android/wallpaper/picker/CategoryFragment.java
@@ -110,7 +110,7 @@ import java.util.List;
 /**
  * Displays the Main UI for picking a category of wallpapers to choose from.
  */
-public class CategoryFragment extends ToolbarFragment
+public class CategoryFragment extends AppbarFragment
         implements CategorySelectorFragmentHost, ThumbnailUpdater, WallpaperDestinationCallback {
 
     private final Rect mPreviewLocalRect = new Rect();
@@ -131,7 +131,7 @@ public class CategoryFragment extends ToolbarFragment
 
     public static CategoryFragment newInstance(CharSequence title) {
         CategoryFragment fragment = new CategoryFragment();
-        fragment.setArguments(ToolbarFragment.createArguments(title));
+        fragment.setArguments(AppbarFragment.createArguments(title));
         return fragment;
     }
 
