da89b3fe5df2d09ad62b6882836a1ca35077f492
Alejandro Nijamkin <nijamkin@google.com>
2022-12-22 15:22:07 -0800

    Unified screen preview section (1/3).
    Implementation of a new section where we show the preview of the lock
    screen and home screen and switch between them.
    
    Fix: 263507648
    Test: manually verified that switching between the lock screen and home
    screen tabs properly updates the preview. Also, scrolling up and down
    faithfully moves the preview view on on the screen without rendering
    artifacts.
    
    Change-Id: Idac6b0d0baaad7e14b9eae5b14eb40d35179f6e9

--- src/com/android/wallpaper/picker/customization/ui/binder/ScreenPreviewBinder.kt
+++ src/com/android/wallpaper/picker/customization/ui/binder/ScreenPreviewBinder.kt
@@ -18,11 +18,13 @@
 package com.android.wallpaper.picker.customization.ui.binder
 
 import android.app.Activity
+import android.app.WallpaperColors
 import android.content.Intent
 import android.os.Bundle
 import android.service.wallpaper.WallpaperService
 import android.view.SurfaceView
 import androidx.cardview.widget.CardView
+import androidx.core.view.isVisible
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
@@ -34,19 +36,22 @@ import com.android.wallpaper.model.LiveWallpaperInfo
 import com.android.wallpaper.model.WallpaperInfo
 import com.android.wallpaper.picker.WorkspaceSurfaceHolderCallback
 import com.android.wallpaper.picker.customization.ui.viewmodel.ScreenPreviewViewModel
-import com.android.wallpaper.util.PreviewUtils
 import com.android.wallpaper.util.ResourceUtils
 import com.android.wallpaper.util.WallpaperConnection
 import com.android.wallpaper.util.WallpaperSurfaceCallback
 import java.util.concurrent.CompletableFuture
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.launch
 
 /**
  * Binds between view and view-model for rendering the preview of the home screen or the lock
  * screen.
  */
+@OptIn(ExperimentalCoroutinesApi::class)
 object ScreenPreviewBinder {
     interface Binding {
+        fun show()
+        fun hide()
         fun sendMessage(
             id: Int,
             args: Bundle = Bundle.EMPTY,
@@ -59,7 +64,6 @@ object ScreenPreviewBinder {
         previewView: CardView,
         viewModel: ScreenPreviewViewModel,
         lifecycleOwner: LifecycleOwner,
-        wallpaperInfoProvider: suspend () -> WallpaperInfo?,
     ): Binding {
         val workspaceSurface: SurfaceView = previewView.requireViewById(R.id.workspace_surface)
         val wallpaperSurface: SurfaceView = previewView.requireViewById(R.id.wallpaper_surface)
@@ -79,13 +83,7 @@ object ScreenPreviewBinder {
                         previewSurfaceCallback =
                             WorkspaceSurfaceHolderCallback(
                                 workspaceSurface,
-                                PreviewUtils(
-                                    context = previewView.context,
-                                    authority =
-                                        viewModel.getContentProviderAuthority(
-                                            previewView.context.applicationContext,
-                                        ),
-                                ),
+                                viewModel.previewUtils,
                                 viewModel.getInitialExtras(),
                             )
                         workspaceSurface.holder.addCallback(previewSurfaceCallback)
@@ -123,7 +121,7 @@ object ScreenPreviewBinder {
                     }
                     Lifecycle.Event.ON_RESUME -> {
                         lifecycleOwner.lifecycleScope.launch {
-                            wallpaperInfo = wallpaperInfoProvider()
+                            wallpaperInfo = viewModel.getWallpaperInfo()
                             (wallpaperInfo as? LiveWallpaperInfo)?.let { liveWallpaperInfo ->
                                 if (WallpaperConnection.isPreviewAvailable()) {
                                     wallpaperConnection =
@@ -136,7 +134,15 @@ object ScreenPreviewBinder {
                                                 )
                                             },
                                             previewView.context,
-                                            null,
+                                            object :
+                                                WallpaperConnection.WallpaperConnectionListener {
+                                                override fun onWallpaperColorsChanged(
+                                                    colors: WallpaperColors?,
+                                                    displayId: Int
+                                                ) {
+                                                    viewModel.onWallpaperColorsChanged(colors)
+                                                }
+                                            },
                                             wallpaperSurface,
                                             null,
                                         )
@@ -164,6 +170,18 @@ object ScreenPreviewBinder {
         )
 
         return object : Binding {
+            override fun show() {
+                previewView.isVisible = true
+                wallpaperSurface.isVisible = true
+                workspaceSurface.isVisible = true
+            }
+
+            override fun hide() {
+                previewView.isVisible = false
+                wallpaperSurface.isVisible = false
+                workspaceSurface.isVisible = false
+            }
+
             override fun sendMessage(id: Int, args: Bundle) {
                 previewSurfaceCallback?.send(id, args)
             }
