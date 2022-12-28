feed6b147b808d0f7d9c8e275e41725c7b97edc0
George Lin <giolin@google.com>
2022-12-28 04:04:24 +0000

    [WPP2] Make injectors kt
    Convert all injectors in wallpaper picker app to Kotlin files
    
    Test: Make sure build and manually run the CUJs
    Bug: 263826701
    Change-Id: If4ad5289e5575bfaad1df2c9c239fde930633874

---
+++ tests/src/com/android/wallpaper/testing/TestCustomizationSections.kt
@@ -0,0 +1,45 @@
+package com.android.wallpaper.testing
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.LifecycleOwner
+import com.android.wallpaper.model.CustomizationSectionController
+import com.android.wallpaper.model.PermissionRequester
+import com.android.wallpaper.model.WallpaperColorsViewModel
+import com.android.wallpaper.model.WallpaperPreviewNavigator
+import com.android.wallpaper.model.WorkspaceViewModel
+import com.android.wallpaper.module.CurrentWallpaperInfoFactory
+import com.android.wallpaper.module.CustomizationSections
+
+/** Test implementation of [CustomizationSections] */
+class TestCustomizationSections : CustomizationSections {
+    override fun getSectionControllersForScreen(
+        screen: CustomizationSections.Screen?,
+        activity: FragmentActivity?,
+        lifecycleOwner: LifecycleOwner?,
+        wallpaperColorsViewModel: WallpaperColorsViewModel?,
+        workspaceViewModel: WorkspaceViewModel?,
+        permissionRequester: PermissionRequester?,
+        wallpaperPreviewNavigator: WallpaperPreviewNavigator?,
+        sectionNavigationController:
+            CustomizationSectionController.CustomizationSectionNavigationController?,
+        savedInstanceState: Bundle?,
+        wallpaperInfoFactory: CurrentWallpaperInfoFactory?
+    ): MutableList<CustomizationSectionController<*>> {
+        return arrayListOf()
+    }
+
+    override fun getAllSectionControllers(
+        activity: FragmentActivity?,
+        lifecycleOwner: LifecycleOwner?,
+        wallpaperColorsViewModel: WallpaperColorsViewModel?,
+        workspaceViewModel: WorkspaceViewModel?,
+        permissionRequester: PermissionRequester?,
+        wallpaperPreviewNavigator: WallpaperPreviewNavigator?,
+        sectionNavigationController:
+            CustomizationSectionController.CustomizationSectionNavigationController?,
+        savedInstanceState: Bundle?
+    ): MutableList<CustomizationSectionController<*>> {
+        return arrayListOf()
+    }
+}
