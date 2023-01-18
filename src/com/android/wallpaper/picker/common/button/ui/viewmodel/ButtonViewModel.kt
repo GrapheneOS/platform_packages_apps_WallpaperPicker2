132c0197520e27efc23effb7b15c3cdfa1586721
Alejandro Nijamkin <nijamkin@google.com>
2023-01-17 17:52:24 -0800

    Generic system for fancy dialogs.
    Implements the latest UX spec for fancy looking dialogs and uses them
    for the "reset" confirmation dialog.
    
    Bug: 265836232
    Test: manually tested the reset confirmation dialog
    Test: unit tests for the undo system still pass
    Change-Id: I2e9b3198ee1c096e4b5181a98b8805309a41682a

--- src/com/android/wallpaper/picker/common/button/ui/viewmodel/ButtonViewModel.kt
+++ src/com/android/wallpaper/picker/common/button/ui/viewmodel/ButtonViewModel.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,10 +15,12 @@
  *
  */
 
-package com.android.wallpaper.picker.undo.ui.viewmodel
+package com.android.wallpaper.picker.common.button.ui.viewmodel
 
-/** Models the UI state for an undo confirmation dialog. */
-data class UndoDialogViewModel(
-    val onConfirmed: () -> Unit,
-    val onDismissed: () -> Unit,
+import com.android.wallpaper.picker.common.text.ui.viewmodel.Text
+
+data class ButtonViewModel(
+    val text: Text,
+    val style: ButtonStyle,
+    val onClicked: (() -> Unit)? = null,
 )
