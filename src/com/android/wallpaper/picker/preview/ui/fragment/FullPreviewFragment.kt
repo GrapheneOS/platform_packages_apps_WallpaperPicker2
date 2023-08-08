4a874c1f21a4722fd6b38906782da5d2c7cb5643
Austin Wang <wangaustin@google.com>
2023-08-07 20:11:16 -0700

    Add new UI flow for multi crop preview (1/2)
    Also add a small preview of user selected wallpaper with one tap to
    launch full preview, which is a placeholder in this CL.
    
    Flag: WALLPAPER_PICKER_MULTI_CROP_PREVIEW
    Bug: 291761856
    Test: launch from gallery, main preview and photo
    Change-Id: If953af0ffc77e8a460b56503a2a794a03c1f8ea8

---
+++ src/com/android/wallpaper/picker/preview/ui/fragment/FullPreviewFragment.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.wallpaper.picker.preview.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.android.wallpaper.R
+import dagger.hilt.android.AndroidEntryPoint
+
+/** Shows full preview of user selected wallpaper for cropping, zooming and positioning. */
+@AndroidEntryPoint(Fragment::class)
+class FullPreviewFragment : Hilt_FullPreviewFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_full_preview, container, false)
+        return view
+    }
+}
