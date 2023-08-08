ded10975d8fcf57174d7f8367517f7929a6acd97
Austin Wang <wangaustin@google.com>
2023-08-07 20:11:16 -0700

    Add new UI flow for multi crop preview (1/2)
    Also add a small preview of user selected wallpaper with one tap to
    launch full preview, which is a placeholder in this CL.
    
    Flag: WALLPAPER_PICKER_MULTI_CROP_PREVIEW
    Bug: 291761856
    Test: launch from gallery, main preview and photo
    Change-Id: If953af0ffc77e8a460b56503a2a794a03c1f8ea8
    Merged-In: If953af0ffc77e8a460b56503a2a794a03c1f8ea8

--- src/com/android/wallpaper/picker/preview/ui/WallpaperPreviewActivity.kt
+++ src/com/android/wallpaper/picker/preview/ui/WallpaperPreviewActivity.kt
@@ -18,20 +18,14 @@ package com.android.wallpaper.picker.preview.ui
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo
-import android.net.Uri
+import android.graphics.Color
 import android.os.Bundle
-import android.transition.Slide
-import android.view.Window
 import android.widget.Toast
 import androidx.activity.viewModels
 import com.android.wallpaper.R
-import com.android.wallpaper.model.ImageWallpaperInfo
 import com.android.wallpaper.model.WallpaperInfo
 import com.android.wallpaper.picker.AppbarFragment
 import com.android.wallpaper.picker.BasePreviewActivity
-import com.android.wallpaper.picker.PreviewFragment
-import com.android.wallpaper.picker.di.navigation.NavigationController
-import com.android.wallpaper.picker.di.navigation.Transition
 import com.android.wallpaper.picker.preview.ui.viewmodel.WallpaperPreviewViewModel
 import com.android.wallpaper.util.ActivityUtils
 import com.android.wallpaper.util.DisplayUtils
@@ -43,84 +37,37 @@ import javax.inject.Inject
 class WallpaperPreviewActivity :
     Hilt_WallpaperPreviewActivity(), AppbarFragment.AppbarFragmentHost {
     private val viewModel: WallpaperPreviewViewModel by viewModels()
-    @Inject lateinit var navigator: NavigationController
     @Inject lateinit var displayUtils: DisplayUtils
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val wallpaper = intent.getParcelableExtra(EXTRA_WALLPAPER_INFO, WallpaperInfo::class.java)
-        viewModel.editingWallpaper = wallpaper
-        val isFullScreen = intent.getBooleanExtra(EXTRA_IS_FULL_SCREEN, false)
-        if (isFullScreen) {
-            window.allowEnterTransitionOverlap = true
-            window.requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
-            window.exitTransition = Slide()
-            window.enterTransition = Slide()
-        }
-        // TODO(b/291761856): create new layout and use here
-        setContentView(R.layout.activity_preview)
-        enableFullScreen()
-        val fragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
-        if (fragment == null) {
-            navigator.navigateToPreview(
-                activity = this,
-                wallpaperInfo = wallpaper,
-                mode = PreviewFragment.MODE_CROP_AND_SET_WALLPAPER,
-                viewAsHome = intent.getBooleanExtra(EXTRA_VIEW_AS_HOME, false),
-                viewFullScreen = false,
-                testingModeEnabled = intent.getBooleanExtra(EXTRA_TESTING_MODE_ENABLED, false),
-                viewId = R.id.fragment_container,
-                transition = Transition.ADD
-            )
-        }
+        window.navigationBarColor = Color.TRANSPARENT
+        window.statusBarColor = Color.TRANSPARENT
+        setContentView(R.layout.activity_wallpaper_preview)
+        viewModel.editingWallpaper =
+            intent.getParcelableExtra(EXTRA_WALLPAPER_INFO, WallpaperInfo::class.java)
     }
 
     override fun onUpArrowPressed() {
-        onBackPressed()
+        onBackPressedDispatcher.onBackPressed()
     }
 
     override fun isUpArrowSupported(): Boolean {
         return !ActivityUtils.isSUWMode(baseContext)
     }
 
-    // TODO(b/292592383): migrate activity result method to latest api callback
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == RESULT_MY_PHOTOS && resultCode == RESULT_OK) {
-            val imageUri = data?.let { it.data as? Uri }
-            if (imageUri != null) {
-                val imageWallpaper = ImageWallpaperInfo(imageUri)
-                navigator.navigateToPreview(
-                    this,
-                    imageWallpaper,
-                    PreviewFragment.MODE_CROP_AND_SET_WALLPAPER,
-                    true,
-                    /* viewFullScreen= */ false,
-                    false,
-                    R.id.fragment_container,
-                    Transition.REPLACE
-                )
-            }
-        }
-    }
-
     override fun onResume() {
         super.onResume()
-        val orientation =
+        requestedOrientation =
             if (displayUtils.isOnWallpaperDisplay(this)) ActivityInfo.SCREEN_ORIENTATION_USER
             else ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-        requestedOrientation = orientation
         if (isInMultiWindowMode) {
             Toast.makeText(this, R.string.wallpaper_exit_split_screen, Toast.LENGTH_SHORT).show()
-            onBackPressed()
+            onBackPressedDispatcher.onBackPressed()
         }
     }
 
     companion object {
-        /** Request code to map activity result for ImageWallpaperInfo */
-        val RESULT_MY_PHOTOS = 0
-        const val EXTRA_IS_FULL_SCREEN = "com.android.wallpaper.picker.is_full_screen"
-
         /**
          * Returns a new [Intent] that can be used to start [WallpaperPreviewActivity].
          *
@@ -140,7 +87,6 @@ class WallpaperPreviewActivity :
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
             }
             intent.putExtra(EXTRA_WALLPAPER_INFO, wallpaperInfo)
-            intent.putExtra(EXTRA_IS_FULL_SCREEN, wallpaperInfo)
             return intent
         }
     }
