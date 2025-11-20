ed7604cd4b34a1c54e7b38c9cd16c7d1ab1f1a91
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-11-21 00:19:59 +0200

    hide the always-empty wallpaper carousel

--- src/com/android/wallpaper/picker/customization/ui/view/WallpaperPickerEntry.kt
+++ src/com/android/wallpaper/picker/customization/ui/view/WallpaperPickerEntry.kt
@@ -69,7 +69,7 @@ constructor(context: Context, attrs: AttributeSet? = null) : FrameLayout(context
     private var collapsedWidth = 0
     private var progress = 1f
     private var animator: ValueAnimator? = null
-    private var state: State = State.EXPANDED
+    private var state: State = State.COLLAPSED
 
     init {
         val layoutResource =
@@ -119,6 +119,7 @@ constructor(context: Context, attrs: AttributeSet? = null) : FrameLayout(context
                         R.dimen.customization_option_container_horizontal_padding
                     ) * 4
             collapsedHeight = collapsedButton.height
+            setProgress(PROGRESS_COLLAPSED)
         }
     }
 
@@ -171,6 +172,7 @@ constructor(context: Context, attrs: AttributeSet? = null) : FrameLayout(context
     }
 
     fun animateToExpanded() {
+        if (true) return
         if (wallpaperCarousel.adapter?.itemCount == 0) return
 
         if (state == State.EXPANDED || state == State.EXPANDING) {
