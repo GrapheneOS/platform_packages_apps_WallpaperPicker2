64a774498c75c88d7d0a87e3734bdddca1b88b58
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2026-06-22 12:11:47 +0000

    Revert "enable integration of the embedded photo picker"
    This flag is no longer checked by the current codepath.

--- src/com/android/wallpaper/config/BaseFlags.kt
+++ src/com/android/wallpaper/config/BaseFlags.kt
@@ -104,7 +104,7 @@ abstract class BaseFlags {
 
     // This is just a local flag in order to ensure right behaviour in case
     // something goes wrong with PhotoPicker integration.
-    open fun isPhotoPickerEnabled() = true
+    open fun isPhotoPickerEnabled() = false
 
     // This flag is to gate the dependency of default recents on new categories
     // fetching logic.
